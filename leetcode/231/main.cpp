8d2ce3862e9117f677962a5da4042bd358e5aafe
yukienomiya <nomiyayukie@gmail.com>
2021-10-21 00:06:24 +0200

    Add problem 231

---
+++ leetcode/231/main.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Solution {
+public:
+  bool isPowerOfTwo(int n) {
+    if (n < 0) return false;
+    int count = 0;
+    while (n) {
+      count += n & 1;
+      n >>= 1;
+    }
+    return count == 1;
+  }
+};
