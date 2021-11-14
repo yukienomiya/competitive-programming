59ba1795640606410f9006328930e8dbdb0fe8de
yukienomiya <nomiyayukie@gmail.com>
2021-11-14 23:54:57 +0100

    Add problem 700

---
+++ leetcode/700/main.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  TreeNode* searchBST(TreeNode* root, int val) {
+    if (root->val == val) return root;
+    if (root->val > val && root->left) return searchBST(root->left, val);
+    else if (root->val < val && root->right) return searchBST(root->right, val);
+    else return nullptr;
+  }
+};
